28de5de6400873fda224480fb9bdddc1dc4e3618
paian <paian@ajou.ac.kr>
2019-01-27 13:53:39 +0900

    add phpmyadmin

--- install-apm.sh
+++ install-apm.sh
@@ -12,4 +12,7 @@ apt-cache search php7
 #optional
 # sudo apt-get install -y libapache2-mod-php7.0 php7.0 php7.0-cgi php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-json php7.0-mbstring php7.0-mysql php7.0-mcrypt  php7.0-xml php7.0-xmlrpc
 
+#phpmyadmin
+sudo apt-get install -y phpmyadmin
+
 echo "install finish" 
